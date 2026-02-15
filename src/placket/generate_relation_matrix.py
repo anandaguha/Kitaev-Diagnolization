606f6ba9d02207e0ffd595f9d8b7565bf0094d9a
Ananda Guha <anandaguha1@gmail.com>
2026-02-15 12:32:38 -0800

    Fixed the relation matrix code to now be square and actually created the matrix needed for diagnlizaition properly

--- src/placket/generate_relation_matrix.py
+++ src/placket/generate_relation_matrix.py
@@ -1,27 +1,96 @@
-from typing import Dict
+from typing import Dict, Tuple
 import numpy as np
-Class relation_table:
-    def __intit__ (self, minimum_tessalation:int, neighbors:int, reflection: bool = False ):
-        if reflection:
-            if neighbors%2 != 0:
-                raise ValueError(f"Neighbor isnt even, reflection not possible")
-            neighbors = neighbors/2
-        rows = minimum_tessalation
-        cols = neighbors
-        self._rows = rows
-        self._cols = cols
-        self._table = np.zeros((rows,cols))
+class Relation_Table:
+    """
+    Goal of this class is to take in a table with the unit cells and then 
+    it will give you a unitary hermitation matrix that you can plug
+    two momentum values into. 
+    """
+    def __init__ (self, neighbor_tabel:np.ndarray, total_bonds:int ,basis_bonds:tuple[int], basis_unit_cell:tuple[tuple[int]], reflection: bool = False ):
+        """
+        You take in 
+        neighbor relation table, 
+        total neighbors,
+        the nearest neighbor bonds you want to use as a basis, 
+        the unit cell basis vectors this needs to be given in terms of the nearest neighbor basis,
+        if it is a symetric  for computation time a possible (not implemented yet)
+        """
+        if reflection and total_bonds%2 != 0 :
+            raise ValueError(f"Neighbor isnt even, reflection not possible")
+        else: 
+            total_bonds = total_bonds/2
+        #mybe something to validate dims of the tessalation pattern
+        self._neighbor_table = neighbor_tabel
+        self._total_bonds:int = total_bonds
+        self._basis_bonds:tuple[int] = basis_bonds
+        self._basis_unit_cell:tuple[tuple[int]] = basis_unit_cell
+        if self._basis_bonds[0] > self._basis_bonds[1]:
+            self._basis_bonds[1],self._basis_bonds[0] = self._basis_bonds[0],self._basis_bonds[1]
+        if not all( 0 < x < self._total_bonds for x  in self._basis_bonds):
+            raise ValueError(f"Given bonds are out of range, pick numbers between 1 and {self._total_bonds}")
+        if len(self._basis_bonds) != 2:
+            raise ValueError(f"This class only work for 2 dim systems!")
+        self._steps_between_basis:int  = abs( self._basis_bonds [0] - self._basis_bonds[1])
+        self._two_pi_over_N = 2 * np.pi / float(self._total_bonds)
     
-    #Public method
-    def update_table(self, tessalation_number: int, bond: int, result: int) -> None:
-        row = tessalation_number - 1
-        col = bond - 1
-        self.table [row,col] = result
-        return None
+    #################
+    # Getter/Setter # 
+    #################
+    @property
+    def basis_bonds():
+        return self._basis_bonds
+    @basis_bonds.setter
+    def basis_bonds(value):
+        if not isinstance(value, tuple):
+            raise ValueError(f"You did not pass a tupel when setting bassis bonds")
+        if len(value) != 2:
+            raise ValueError(f"This class only works for 2 dim! Please make sure you tupel is of len 2")
+        if any(self._total_bonds < x <= 0   for x in value):
+            raise ValueError(f"You passed values above or below the accepted range is 1 to {self._total_bonds}")
+        if value[0] > value[1]:
+            self._basis_bonds[1],self._basis_bonds[0] = value[0],value[1]
+        else: 
+            self._basis_bonds = value
+    
+    #Private method
+    def _decompose_bond(self, bond:int) -> Tuple [float,float]:
+        steps_between_first_and_xbond = abs(bond - self._basis_bonds[0])
         
-            
-            
+        coefA_numerator = np.sin(self._two_pi_over_N * (self._steps_between_basis - steps_between_first_and_xbond))
+        coefA_denom = np.sin(self._two_pi_over_N * self._steps_between_basis)
+        coefA = coefA_numerator/coefA_denom
         
+        coefB_numerator = np.sin(self._two_pi_over_N * steps_between_first_and_xbond)
+        coefB_denom = np.sin(self._two_pi_over_N * self._steps_between_basis)
+        coefB = coefB_numerator/coefB_denom
         
+        return (coefA,coefB)
+
+    #TODO: fix itterator for loop !
+    #public method
+    def create_momentum_transform (self):
+        table = np.zeros (self._total_bonds,self._total_bonds)
+        
+        def functional_momentum_matrix(k1,k2):
+            for (row_idx, col_idx), value in np.ndenumerate(self._neighbor_table):
+                current_neighbor:int = row_idx
+                destination_neighbor:int = value
+                bond:int = col_idx
+                
+                coefA,coefB = self._decompose_bond(bond)
+                unit_cell_bond_one_neighbor_basis = self._basis_unit_cell[0]
+                unit_cell_bond_two_neighbor_basis = self._basis_unit_cell[1]
+                
+                bond_vector_neighbor_basis = np.ndarray([coefA],[coefB])
+                neighbor_basis_to_unit_cell_basis = np.ndarray([unit_cell_bond_one_neighbor_basis[0],unit_cell_bond_one_neighbor_basis[1]],
+                                                               [unit_cell_bond_two_neighbor_basis[0], unit_cell_bond_two_neighbor_basis[1]],
+                                                               )
+                inverse_neighbor_to_unit_cell = np.linalg.inv(neighbor_basis_to_unit_cell_basis)
+                new_coef = bond_vector_neighbor_basis @ inverse_neighbor_to_unit_cell
+                table[current_neighbor, destination_neighbor] += np.exp(1j * (k1*new_coef[0] + k2*new_coef[1]))
+            return table
+        return functional_momentum_matrix
+
+
     
         
