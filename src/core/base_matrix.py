6dec4c1428cbcc45ee0bc625f5b71b120ce65644
anandaguha <anandaguha1@gmail.com>
2026-08-31 11:04:20 -0700

    PhaseSpace:WpField:SymbolicNumericalCoupling Chnaged how this function works so that it properly takes in meshgrids. This function would previosly only work on single values, but as soon as you input a meshgrid for a symbol it would crash because the scalars would not exapnd causing a jagged array error. Added broadcasting so this would not happen and everything would inflate to the right size, and then reshape to the correct final shape

--- src/core/base_matrix.py
+++ src/core/base_matrix.py
@@ -22,7 +22,7 @@ class BaseMatrix():
         if isinstance(vec, (str, sp.Expr)):
             vec_str = str(vec)
             if vec_str.startswith("-"):            # If the input is a string, generate D symbols (1-indexed: e.g., p1, p2)
-                syms = sp.symbols(f'{vec_str[1:]}1:{D+1}')
+                syms = sp.symbols(f'{vec_str[1:]}1:{D+1}', real=True)
                 return [-s for s in syms]
             else:
                 return  sp.symbols(f'{vec_str}1:{D+1}')
@@ -35,7 +35,7 @@ class BaseMatrix():
     
     @staticmethod
     def check_hermitian (hermetian_matrix):
-        if not np.allclose(hermetian_matrix, hermetian_matrix.conj().transpose(0, 2, 1), atol=1e-5):
+        if not np.allclose(hermetian_matrix, np.moveaxis(hermetian_matrix, [-2, -1], [-1, -2]).conj(), atol=1e-5): 
             logger.warning("Warning: Matrices are not strictly Hermitian! Imaginary leaks detected.")
         return None
 
@@ -116,29 +116,27 @@ class BaseMatrix():
     @staticmethod
     def symbolic_to_numerical_matrix(symbolic_matrix, dict_of_symbols: Dict[Any, Any], number_of_dim:Optional[int] = None) -> np.ndarray:
         
+        #0. Get all the constants and do your checks
         tuple_of_symbols = tuple(dict_of_symbols.keys())
-
-        # 1. Convert symbolic -> numpy computational using lambdify
-        uninit_compute_matrix = lambdify(tuple_of_symbols, symbolic_matrix, modules='numpy')
-        #intilize the matrix with full meshgrids to create a "stack" of usable matrixs
-        unformated_stacked_computational_matrix = uninit_compute_matrix(*dict_of_symbols.values())
-        ndims = unformated_stacked_computational_matrix.ndim
-        if number_of_dim != None and number_of_dim != ndims:
-            logger.warning(f"\nThe number of dim's you provided is not matching the number of dims found!\nGiven dims: {number_of_dim}\nActual dims:{ndims}\n")
-        dynamic_axis = list(range(2,ndims)) + [0,1]
-
-        # 2. Format the matrix correctly so np.eiginvals can compute it properly 
-        # Make sure you moveaxis/transpose -> reshape! RESHAPING should always happen last for speed!!
-        # aka transpose then flatten
-        transposed_stacked_computation_matrix = np.transpose(unformated_stacked_computational_matrix,axes=dynamic_axis) #puts all the actual values that we squished into one list at the front, this is how np.eginvalues needs it. The row and column are just basically metadata that tell np how to interpert this massive list of values
-        flat_stacked_computation_matrix = transposed_stacked_computation_matrix.reshape(-1,transposed_stacked_computation_matrix.shape[-2],transposed_stacked_computation_matrix.shape[-1]) #-1 means "figure out the rest" this keeps the row and columns and basically squishes the rest of the value into one list
-        imaginary_flat_stacked_computational_matrix = np.asarray(flat_stacked_computation_matrix, dtype=np.complex128) #convert to complex array just for consietensiy before passing it to the solver
-
-        # 4. do checks to make sure nothing went wrong
-        BaseMatrix.check_hermitian(imaginary_flat_stacked_computational_matrix) 
-
-        # 5. return the fully processed matrix to be computated 
-        return imaginary_flat_stacked_computational_matrix 
+        og_shape = symbolic_matrix.shape[0]
+        meshgrid_shape = list(dict_of_symbols.values())[0].shape #gives you the full shape
+        if number_of_dim != None and number_of_dim != len(meshgrid_shape):
+            logger.warning(f"\nThe number of dim's you provided is not matching the number of dims found!\nGiven dims: {number_of_dim}\nActual dims:{len(meshgrid_shape)}\n")
+        
+        # 1. Convert symbolic -> FLAT numpy computational using lambdify
+        uninit_compute_matrix = lambdify(tuple_of_symbols, tuple(symbolic_matrix), modules='numpy')
+        # 2. Input your mesgrid to expand out the terms with the INPUT Symbols
+        uninit_compute_matrix_expanded = uninit_compute_matrix(*dict_of_symbols.values())
+        # 3. Broadcast the largest array to all values so they also become that size array (turns integers into a matrix of just that number)
+        uninit_compute_matrix_expanded_broadcasted = np.broadcast_arrays(*uninit_compute_matrix_expanded)
+        # 4. Reshape the matrix now that that everything is the same size
+        stacked_numerical_matrix = np.reshape(uninit_compute_matrix_expanded_broadcasted, (og_shape,og_shape,*meshgrid_shape))
+        # 5. Transpose since your current shape is (Site,Site,Number of inputs, Number of inputs,...,Number of inputs) and the Number of inputs needs to be in the front for np.linalg.eigen
+        stacked_numerical_matrix_transposed = np.moveaxis(stacked_numerical_matrix, [0,1] ,[-2,-1])
+        # 5. Check your result
+        BaseMatrix.check_hermitian(stacked_numerical_matrix_transposed)
+        #6. Return your matrix 
+        return stacked_numerical_matrix_transposed
 
     @staticmethod 
     def sum_neg_eignvalues (matrix):
